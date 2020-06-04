ab12e9d91778eddcf39355212e13e7908b01d04d
tcitry <tcitry@gmail.com>
2020-06-05 01:29:55 +0800

    add filter_fields

--- api_permission/admin.py
+++ api_permission/admin.py
@@ -5,4 +5,6 @@ from .models import APIPermissionModel
 @admin.register(APIPermissionModel)
 class ApiPermissionAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at', 'updated_at']
-    list_display = ['pattern', 'method', 'active', 'created_at']
+    list_display = ['pattern', 'method', 'active', 'comment', 'created_at']
+    list_filter = ['method', 'active']
+    search_fields = ['pattern', 'comment']
